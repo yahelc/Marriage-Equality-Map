268b54a3970dffecdc44d7c97c8e2d1833e95b3f
Yahel Carmon <yahelc@gmail.com>
2014-05-15 08:23:24 -0700

    Revert "Belated moving Arkansas to the equality column"
    This reverts commit cad779a3bbada433f1d98c08a4c1dbb73d8ce720.

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ jQuery.fn.convertToPercentString = function(data) {
 };
 
 var hasEquality = ["ny", "ia", "wa", "me", "nh", "ma", "ct", "md", "dc", 
-	"vt", "ri", "de", "mn", "ca", "nj", "il", "hi", "nm", "ar"];
+	"vt", "ri", "de", "mn", "ca", "nj", "il", "hi", "nm"];
 var constitutionallyBanned = ["mt", "id", "ut", "az", "ak", "nd", "sd", 
-"ne", "ks", "ok", "tx", "la","mo", "mi", "oh", "ky", "tn", "ms", 
+"ne", "ks", "ok", "tx", "la", "ar", "mo", "mi", "oh", "ky", "tn", "ms", 
 "al", "tn", "ga",  "fl", "sc", "nc", "va", "co", "or", "wi", "nv"];
 var legislativelyBanned = ["wy", "in", "wv", "pa"];
 var colors = {"equality":"#21799C", "ban": "darkred", "law": "#FC5B5B"};
